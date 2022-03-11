e741f2bc7fd8b4ffcffcfe30889c15616069910f
Kyle Taylor <kyletaylored@gmail.com>
2022-03-11 13:00:53 -0600

    Update site install

--- web/private/scripts/drush_site_install.php
+++ web/private/scripts/drush_site_install.php
@@ -7,5 +7,5 @@ $title = $meta['label'];
 $email = $_POST['user_email'];
 
 // Install from profile.
-echo "Installing default profile...\n";
-passthru('drush site:install commerce_kickstart --account-mail="' . $email . '" --site-name="' . $title . '" --account-name="superuser" -y > /dev/null 2>&1 &');
+echo "Installing Commerce Kickstart profile...\n";
+passthru('drush site:install commerce_kickstart --account-mail="' . $email . '" --site-name="' . $title . '" --account-name="superuser" -y && drush en commerce_kickstart_basic_catalog -y');
